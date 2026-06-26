5ac3271752b3818de00bc4dac6c3477f40b6608c
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-06-26 12:16:05 +0500

    fix: correct typo in mongoose connection state check

--- 02-setup-local-redis/src/index.js
+++ 02-setup-local-redis/src/index.js
@@ -14,10 +14,9 @@ app.get("/redis" , async(req , res)=> {
 app.get("/mongo" , async(req , res)=> {
     const url = process.env.MONGO_URL || "mongodb://localhost:27017/chai_aur_redis"
 
-    if(mongoose.connection.readyState === 0){
+    if(mongoose.connection.readystate === 0){
         await mongoose.connect(url)
     }
-
     res.json({mongo : "connected" , database : mongoose.connection.name})
 });
 
