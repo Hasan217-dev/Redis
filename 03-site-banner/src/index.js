7c10d91bcecf2f18c72ed1e3efa34bbcdcf06f7c
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-06-26 12:39:45 +0500

    fix: correct typo in welcome message and streamline code formatting in index.js

--- 03-site-banner/src/index.js
+++ 03-site-banner/src/index.js
@@ -1,32 +1,31 @@
 import express from 'express';
 import Redis from 'ioredis';
 
-
 const app = express();
 app.use(express.json())
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
-const BANNER_KEY = "app:banner";
+const BANNER_KEY = "app:banner"
 
-app.post("/banner" , async(req , res)=> {
-    await redis.set(BANNER_KEY , req.body.message || "Welcomw to our website")
-    res.json({success : true})
+app.post("/banner" , async (req , res)=>{
+   await redis.set(BANNER_KEY , req.body.message || "Welcome to our website")
+   res.json({success : true})
 });
 
 app.get("/banner" , async(req , res)=>{
-    const message = await redis.get(BANNER_KEY)
+   const message = await redis.get(BANNER_KEY);
     res.json({message})
 });
 
-app.delete("/banner" , async(req , res)=> {
+app.delete("/banner" , async(req , res)=>{
     await redis.del(BANNER_KEY)
-    res.json({success : true})
+    res.json({message: true})
 });
 
-app.get("/banner/exists" , async(req , res)=>{
+app.get("/banner/exists" , async (req , res)=>{
    const exists = await redis.exists(BANNER_KEY)
-   res.json({exists : Boolean(exists)})
+    res.json({exists : Boolean(exists)})
 });
 
 app.listen(3000 , ()=>{
